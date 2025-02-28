caf0d8fafcd4dd41c56bcbd0c045d53ac308f21e
Fahmousss <09031382227177@student.unsri.ac.id>
2025-02-28 16:17:09 +0700

    feat: implement HasName trait on User model - Add `HasName` trait to enhance name-related functionality in the User model - Ensure compatibility with Filament features

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\HasName;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasName
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -44,5 +45,17 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Get the formatted Filament name.
+     *
+     * This method converts the model's name into a headline-style string.
+     *
+     * @return string The formatted Filament name.
+     */
+    public function getFilamentName(): string
+    {
+        return Str::headline($this->name);
     }
 }
