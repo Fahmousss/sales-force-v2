80804a2aa96fa80c9874df6b8d24e9b7fdba0529
Fahmousss <09031382227177@student.unsri.ac.id>
2025-02-28 16:40:06 +0700

    chore: update Filament Shield configuration - Disable `panel_user` by setting `enabled` to `false` - Remove unnecessary permission prefixes: `restore_any`, `replicate`, `reorder`, `force_delete`, `force_delete_any` - Simplify role and permission management

--- config/filament-shield.php
+++ config/filament-shield.php
@@ -27,7 +27,7 @@ return [
     ],
 
     'panel_user' => [
-        'enabled' => true,
+        'enabled' => false,
         'name' => 'panel_user',
     ],
 
@@ -38,13 +38,8 @@ return [
             'create',
             'update',
             'restore',
-            'restore_any',
-            'replicate',
-            'reorder',
             'delete',
             'delete_any',
-            'force_delete',
-            'force_delete_any',
         ],
 
         'page' => 'page',
@@ -72,7 +67,8 @@ return [
         ],
 
         'widgets' => [
-            'AccountWidget', 'FilamentInfoWidget',
+            'AccountWidget',
+            'FilamentInfoWidget',
         ],
 
         'resources' => [],
