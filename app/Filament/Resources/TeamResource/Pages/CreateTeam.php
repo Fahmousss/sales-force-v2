4f2a077bcfb41a52435b9e183d3595328ee2d2bc
Fahmousss <09031382227177@student.unsri.ac.id>
2025-03-01 22:38:53 +0700

    feat: enhance team creation validation in CreateTeam
    - Disabled "Create Another" option by setting `$canCreateAnother = false`.
    - Added redirect to the team index page after creation.
    - Implemented `beforeCreate()` validation to prevent a Head Office (`KANTOR_PUSAT`) from having a parent team, displaying an error notification if the condition is violated.

--- app/Filament/Resources/TeamResource/Pages/CreateTeam.php
+++ app/Filament/Resources/TeamResource/Pages/CreateTeam.php
@@ -2,11 +2,34 @@
 
 namespace App\Filament\Resources\TeamResource\Pages;
 
+use App\Enums\TeamType;
 use App\Filament\Resources\TeamResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateTeam extends CreateRecord
 {
     protected static string $resource = TeamResource::class;
+
+    protected static bool $canCreateAnother = false;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function beforeCreate(): void
+    {
+        if (auth()->user()->team->type === TeamType::KANTOR_PUSAT && auth()->user()->team->parent_key !== null) {
+            Notification::make()
+                ->danger()
+                ->title('Invalid Team Structure!')
+                ->body('A Head Office should not have a parent team.')
+                ->persistent()
+                ->send();
+
+            $this->halt();
+        }
+    }
 }
